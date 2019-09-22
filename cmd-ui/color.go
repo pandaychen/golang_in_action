1518ef371ab89be259dd4ea80d5a749ddaf0d8c4
pandaychen <panda.chenyong@gmail.com>
2019-09-22 10:12:27 +0000

    fix color

--- cmd-ui/color.go
+++ cmd-ui/color.go
@@ -13,6 +13,15 @@ func main() {
 	text := 31 // 前景色、红色
 	fmt.Printf("\n %c[%d;%d;%dm%s%c[0m\n\n", 0x1B, conf, bg, text, "testPrintColor", 0x1B)
 
-	str := SetColor("pandaychen", 1, 40, 31)
+	str := SetColor("pandaychen", 1, 41, 31)
 	fmt.Println(str)
+	for b := 40; b <= 47; b++ { // 背景色彩 = 40-47
+		for f := 30; f <= 37; f++ { // 前景色彩 = 30-37
+			for d := range []int{0, 1, 4, 5, 7, 8} { // 显示方式 = 0,1,4,5,7,8
+				fmt.Printf(" %c[%d;%d;%dm%s(f=%d,b=%d,d=%d)%c[0m ", 0x1B, d, b, f, "", f, b, d, 0x1B)
+			}
+			fmt.Println("")
+		}
+		fmt.Println("")
+	}
 }
